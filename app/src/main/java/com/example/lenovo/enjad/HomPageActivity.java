026652ba58132032bf0316c5d1e56d6aec1e86a5
Njoud <njoudghannam@outlook.com>
2017-10-23 17:01:07 +0300

    Njoud added home page connected to profile page and chatlist page

---
+++ app/src/main/java/com/example/lenovo/enjad/HomPageActivity.java
@@ -0,0 +1,35 @@
+package com.example.lenovo.enjad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hom_page);
+        Button ReportsB = (Button) findViewById(R.id.Reportsbutton);
+        Button ProfileB = (Button) findViewById(R.id.Profilebutton);
+        Button ChatsB = (Button) findViewById(R.id.Chatsbutton);
+
+        ProfileB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent R = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(R);
+            }
+        });
+
+        ChatsB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent chls = new Intent(getApplicationContext(), chatlistActivity.class);
+                startActivity(chls);
+            }
+        });
+    }
+}
