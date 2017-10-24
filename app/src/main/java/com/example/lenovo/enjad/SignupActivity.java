45169ff0bdc2c5e0867c0ba6c71c2ed24998308f
Amjad15 <amj85.95@gmail.com>
2017-10-24 20:41:20 +0300

    تعريب الواجهات + ربط واجهات AMJAD

--- app/src/main/java/com/example/lenovo/enjad/SignupActivity.java
+++ app/src/main/java/com/example/lenovo/enjad/SignupActivity.java
@@ -1,7 +1,9 @@
 package com.example.lenovo.enjad;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+    }
+
+    public void signuponclick(View view) {
+
+        Intent signup = new Intent (this,HomPageActivity.class);
+
+        startActivity(signup);
     }
 }
