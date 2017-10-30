0691a1cc47adb885aaf50673280ca8d79e2c1732
Amjad15 <amj85.95@gmail.com>
2017-10-30 19:56:50 +0300

    amjad 2nd commet: 2 new files related to db

--- app/src/main/java/com/example/lenovo/enjad/LoginActivity.java
+++ app/src/main/java/com/example/lenovo/enjad/LoginActivity.java
@@ -5,22 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button bulogin; EditText usernameet, passwordet;
+    TextView toregister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button bulogin= (Button) findViewById(R.id.loginbu);
+        bulogin= (Button) findViewById(R.id.loginbu);
+        usernameet= (EditText) findViewById(R.id.usernameet);
+        passwordet= (EditText) findViewById(R.id.Passwordet);
+        toregister=(TextView) findViewById(R.id.toregistertv);
+
+        toregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+            }});
 
     }
 
     public void loginonclick(View view) {
+        String username= usernameet.getText().toString(); //take the values of edit text
+        String password= passwordet.getText().toString();
+        String type="login";
 
-        Intent login = new Intent (this,HomPageActivity.class);
+        //loginDataLoaderBW backgrandworker = new loginDataLoaderBW(this);
+        //backgrandworker.execute(type,username,password);
 
-        startActivity(login);
+        //Intent login = new Intent (this,HomPageActivity.class);
+
+        // startActivity(login);
 
 
     }
--- app/src/main/java/com/example/lenovo/enjad/SignupActivity.java
+++ app/src/main/java/com/example/lenovo/enjad/SignupActivity.java
@@ -4,19 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+    Button busign; EditText usernameet, passwordet , emailet, reenterpasset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        busign= (Button) findViewById(R.id.signbu);
+        usernameet= (EditText) findViewById(R.id.usrnameet);
+        passwordet= (EditText) findViewById(R.id.passwrdet);
+        emailet= (EditText) findViewById(R.id.emailet);
+        reenterpasset=(EditText) findViewById(R.id.reenterpasset);
+
+
     }
 
     public void signuponclick(View view) {
 
-        Intent signup = new Intent (this,HomPageActivity.class);
+        String username= usernameet.getText().toString();
+        String password= passwordet.getText().toString();
+        String email= emailet.getText().toString();
+        String type="signup";
 
-        startActivity(signup);
+
+        // registerDataLoaderBW bw = new registerDataLoaderBW(this);
+        // bw.execute(type,username,password,email);
+        //Intent signup = new Intent (this,HomPageActivity.class);
+
+        //startActivity(signup);
     }
 }
